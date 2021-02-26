572ca8ccabcd855db5d5671aee02f88f3e0551fb
ayumitanaka <tanakayumi13@gmail.com>
2021-02-26 12:45:35 -0800

    added forms.py

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
+from .models import Todo
 
 # Create your views here.
 def home(request):
@@ -56,4 +58,23 @@ def loginuser(request):
             return redirect('currenttodos')
 
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.all()
+    return render(request, 'todo/currenttodos.html', { 'todos':todos })
+
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', { 'form':TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            request,
+            'todo/createtodo.html',
+            {
+                'form': TodoForm(),
+                'error': 'Error!'
+            }
